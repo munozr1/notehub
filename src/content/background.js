2d9987d87e7fb6bdc0e93d60fecd63dbab57f224
Rodrigo Munoz <rm117579@gmail.com>
2024-06-18 21:59:44 -0500

    feat: started github auth flow, finished ui

--- src/content/background.js
+++ src/content/background.js
@@ -1,12 +1,53 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.scripting.registerContentScripts([{
-    id: "session-script",
-    js: ["./src/content/notion.js"],
-    persistAcrossSessions: false,
-    matches: ["https://www.notion.so/*"],
-    runAt: "document_end",
-  }])
-  .then(() => console.log("Content script registration complete"))
-  .catch((err) => console.warn("Unexpected error", err));
-});
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    switch(request.state ) {
+      case "INITAUTH":
+        startGithubAuthentication(sendResponse);
+        break;
+    }
+    return true;
+  }
+);
+
+
+
+
+async function startGithubAuthentication(callback) {
+  try {
+    const { githubAuthRequestResponse: prev_data } = await chrome.storage.local.get(["githubAuthRequestResponse"]);
+    const clientID = "Iv23lieCe6IdGN8ziPP2";
+    const url = "https://github.com/login/device/code";
+
+    // Check if prev_data is not empty and not expired
+    if (prev_data && prev_data.expires_in > Date.now()) {
+      console.log("using previous data");
+      callback(prev_data);// send to notion
+      return prev_data;
+    }
+
+    console.log("fetching new data");
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        client_id: clientID,
+        scope: "repo",
+      }),
+    });
+
+    const data = await res.json();
+    data.expires_in = Date.now() + data.expires_in * 1000;
+    await chrome.storage.local.set({ githubAuthRequestResponse: data });
+    callback(prev_data); // send to notion
+    return data;
+  } catch (error) {
+    console.log("error: ", error);
+    callback(error);
+    return { error: error.message };
+  }
+}
 
